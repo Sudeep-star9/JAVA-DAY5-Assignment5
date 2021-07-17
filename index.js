42df50b8cab3316ec40d64475eb3bd20ad15e80e
sudeep-star9 <adhikarisudeep12@gmail.com>
2021-07-17 05:50:18 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let text="Cbr, Benelli,Ducati,Caferacer";
+let postion=text.slice(5,12)
+console.log(postion)
+
+
+let tex="Cbr, Benelli,Ducati,Caferacer";
+let postio=tex.slice(-24,-17);
+console.log(postio);
